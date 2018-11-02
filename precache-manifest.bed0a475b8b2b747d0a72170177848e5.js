8b93ba78e828b539863a3d10b272ccc1e1bf869b
Anurag Kumar <anuragak@users.noreply.github.com>
2018-11-02 10:06:33 +0000

    Updates

---
+++ precache-manifest.bed0a475b8b2b747d0a72170177848e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c9e35f12a4090072917fb714c7159ea",
+    "url": "/weather-app/static/media/bg.6c9e35f1.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "a2da4ab9b7da8439c3dc",
+    "url": "/weather-app/static/js/main.a2da4ab9.chunk.js"
+  },
+  {
+    "revision": "84b2767b4372b60cd641",
+    "url": "/weather-app/static/js/1.84b2767b.chunk.js"
+  },
+  {
+    "revision": "a2da4ab9b7da8439c3dc",
+    "url": "/weather-app/static/css/main.9f98770c.chunk.css"
+  },
+  {
+    "revision": "48d53eb3db9eceec43d93e9e6278f806",
+    "url": "/weather-app/index.html"
+  }
+];
